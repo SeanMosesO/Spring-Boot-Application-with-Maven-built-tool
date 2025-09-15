a8133828ecd929b4a9f6af1bb4bc8c4b232ad770
SeanMosesO <124736116+SeanMosesO@users.noreply.github.com>
2025-09-15 10:05:08 +0100

    Add files via upload

---
+++ Environment-Setup/src/main/java/com/seanmotech/Environment/Setup/EnvironmentSetupApplication.java
@@ -0,0 +1,13 @@
+package com.seanmotech.Environment.Setup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvironmentSetupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvironmentSetupApplication.class, args);
+	}
+
+}
---
+++ Environment-Setup/src/test/java/com/seanmotech/Environment/Setup/EnvironmentSetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seanmotech.Environment.Setup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentSetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
